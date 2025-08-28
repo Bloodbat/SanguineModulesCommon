29a25f2b547ae274c5534d645c53b194c692f227
Bloodbat <5965348+Bloodbat@users.noreply.github.com>
2025-08-28 16:02:51 -0600

    Add: sanguinerandom: SanguineRandomNormal: A random generator class which produces floats using normal distribution and PCG32.

--- src/sanguinerandom.hpp
+++ src/sanguinerandom.hpp
@@ -1,6 +1,13 @@
 #pragma once
 
+#include <random>
+
 #include "rack.hpp"
+
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-but-set-parameter"
+#include "pcg_random.hpp"
+#pragma GCC diagnostic pop
 
 namespace sanguineRandom {
 #ifdef METAMODULE
@@ -44,5 +51,23 @@ namespace sanguineRandom {
 
     private:
         uint32_t rng_state_;
+    };
+
+    // A random generator class which produces floats using normal distribution and PCG32.
+    class SanguineRandomNormal {
+    public:
+        void init(const uint64_t seed, const float mean = 0.f, const float deviation = 1.f) {
+            pcgRng = pcg32(seed);
+
+            normalDist = std::normal_distribution<float>(mean, deviation);
+        }
+
+        inline float normal() {
+            return normalDist(pcgRng);
+        }
+
+    private:
+        std::normal_distribution<float> normalDist;
+        pcg32 pcgRng;
     };
 }
